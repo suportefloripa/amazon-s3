ededffbbfd332c3f8540db52ad0e4f265a7caf83
phpclicks <phpclicks@gmail.com>
2016-05-04 15:10:19 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+   // Include the SDK using the Composer autoloader
+     require 'vendor/autoload.php';
+     use Aws\S3\S3Client;
+     use Aws\S3\Exception\S3Exception;
+ 
 include('image_validation.php'); // getExtension Method
 $message='';
 if($_SERVER['REQUEST_METHOD'] == "POST")
@@ -7,7 +12,7 @@ $name = $_FILES['file']['name'];
 $size = $_FILES['file']['size'];
 $tmp = $_FILES['file']['tmp_name'];
 $ext = getExtension($name);
-
+ 
 if(strlen($name) > 0)
 {
 // File format validation
@@ -19,32 +24,38 @@ if($size<(1024*1024))
 include('config_s3.php');
 //Rename image name.
 $image_name_actual = time().".".$ext;
-
-if($s3->putObjectFile($tmp, $bucket , $image_name_actual, S3::ACL_PUBLIC_READ) )
-{
+ 
+ try {
+        $client->putObject(array(
+             'Bucket'=>$bucket,
+             'Key' =>  $image_name_actual,
+             'SourceFile' => $tmp,
+             'StorageClass' => 'REDUCED_REDUNDANCY'
+        ));
 $message = "S3 Upload Successful.";
 $s3file='http://'.$bucket.'.s3.amazonaws.com/'.$actual_image_name;
 echo "<img src='$s3file'/>";
 echo 'S3 File URL:'.$s3file;
-}
-else
-$message = "S3 Upload Fail.";
-
-}
+ 
+    } catch (S3Exception $e) {
+         // Catch an S3 specific exception.
+        echo $e->getMessage();
+    }
+ 
 else
 $message = "Image size Max 1 MB";
-
+ 
 }
 else
 $message = "Invalid file, please upload image file.";
-
+ 
 }
 else
 $message = "Please select image file.";
-
+ 
 }
 ?>
-
+ 
 //HTML Code
 <form action="" method='post' enctype="multipart/form-data">
 Upload image file here
